9b7eb40f7c1da962b5eedbd52f906019328739a9
Steve Falcigno <steve1281@hotmail.com>
2015-01-03 18:13:22 -0500

    finished with loops, context

--- scratch_pad.py
+++ scratch_pad.py
@@ -222,8 +222,127 @@ if __name__ == "__main__":
 """
 
 # this bootstrapping works because if
-# a module is loaded from interupter __name__ is set
+# a module is loaded from interpreter __name__ is set
 # if imported, it is not.
+
+# usual if else exists
+x = int(input("enter x"))
+if x < 9:
+    print("x is a low value")
+elif x >99:
+    print("x is a large value")
+else:
+    print("x is just right")
+
+# another way
+
+x = x + 1 if x < 10 else 0
+# is same as
+if x < 10:
+    x += 1
+else:
+    x = 0
+
+# iteration
+# while loop
+
+x = True
+while x:
+    command = input('Enter a command[rwq]')
+    if 'q' in command.lower(): x = False
+    if command.lower() == 'r':
+        print("Reading...")
+    elif command.lower()=='w':
+        print("Writing...")
+    else:
+        print('Invalid command {0}. Try again.'.format(command))
+else:
+    print("All done")
+
+"""
+Enter a command[rwq]>? r
+Reading...
+Enter a command[rwq]>? w
+Writing...
+Enter a command[rwq]>? f
+Invalid command f. Try again.
+Enter a command[rwq]>? q
+Invalid command q. Try again.
+All done
+"""
+
+
+# for loop
+for item in [1,2,3,4,5]:
+    print("On item {0}".format(item))
+else:
+    print("All Done.")
+"""
+On item 1
+On item 2
+On item 3
+On item 4
+On item 5
+All Done.
+"""
+
+# enumeration
+for number, line in enumerate(open('myfile.txt')):
+    print(number,'\t',line)
+
+"""
+0 	 Line 0 : this is a bunch of lines in a file
+
+1 	 Line 1 : this is a bunch of lines in a file
+
+2 	 Line 2 : this is a bunch of lines in a file
+
+3 	 Line 3 : this is a bunch of lines in a file
+
+4 	 Line 4 : this is a bunch of lines in a file
+
+5 	 Line 5 : this is a bunch of lines in a file
+"""
+
+#generator expression
+[n*n for n in range(1,11) if not n*n %2]
+[4, 16, 36, 64, 100]
+
+# exceptions
+"""
+try
+except
+else
+finally
+"""
+
+# raising your own exception:
+
+try:
+    raise ValueError('wrong value')
+    except ValueError as error:
+    print (error.args)
+
+#the above thing from the book is not working.
+#from the online documentation we get this example
+
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
+"""
+Please enter a number: >? Billy
+Oops!  That was no valid number.  Try again...
+Please enter a number: >? 34
+
+
+"""
+
+# managing context
+
 
 
 
